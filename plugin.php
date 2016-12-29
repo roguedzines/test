b2a16f29b08115a9e301998c89b2c2959d25233e
Li-Shann N. Tomchik <LTomchik@web.int>
2016-12-29 17:09:22 -0500

    created plugin file

---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Weiss Custom
+ * @version 1.0
+ */
+/*
+Plugin Name: Weiss Custom Plugin
+Plugin URI: http://weissinc.com
+Description: A plugin for custom setup for Weiss
+Author: Li-Shann Tomchik
+Version: 1.6
+Author URI: http://weissinc.com
+*/
