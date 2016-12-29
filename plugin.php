b0ca34fd3274b68ea1ecf941060448af4772351e
Li-Shann Tomchik <lili@roguedzines.com>
2016-12-29 17:25:10 -0500

    changed version#

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@
 /*
 Plugin Name: Weiss Custom Plugin
 Plugin URI: http://weissinc.com
-Description: A plugin for custom setup for Weiss
+Description: A plugin for custom setup for Weiss 
 Author: Li-Shann Tomchik
 Version: 1.6
 Author URI: http://weissinc.com
