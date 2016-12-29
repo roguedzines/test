f5c1f681f0aab71c5ff6326dd66b3d9c3b630697
Li-Shann Tomchik <lili@roguedzines.com>
2016-12-29 17:28:18 -0500

    redo

--- plugin.php
+++ plugin.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * @package Weiss Custom
- * @version 1.0
+ * @version 1.1
  */
 /*
 Plugin Name: Weiss Custom Plugin
 Plugin URI: http://weissinc.com
-Description: A plugin for custom setup for Weiss 
+Description: A plugin for custom setup for Weiss
 Author: Li-Shann Tomchik
 Version: 1.6
 Author URI: http://weissinc.com
