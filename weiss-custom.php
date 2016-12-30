defe69fe7807b73571071d5f6672091db93b3c41
Li-Shann Tomchik <lili@roguedzines.com>
2016-12-30 12:15:34 -0500

    updated options page

--- weiss-custom.php
+++ weiss-custom.php
@@ -25,13 +25,40 @@ function weisscp_install() {
     deactivate_plugins( basename( __FILE__ ) ); // Deactivate our plugin
   }
 }
-add_action( 'admin_menu', 'weisscp_admin_settings_page' );
-function weisscp_admin_settings_page() {
-  add_options_page(
-    'Weiss Settings',
-    'Weiss Settings',
-    'manage_options',
-    'weisscp_admin_settings',
-    'weisscp_admin_settings_page'
-   );
+// add_action( 'admin_menu', 'weisscp_admin_settings_page' );
+// function weisscp_admin_settings_page() {
+//   add_options_page(
+//     'Weiss Settings',
+//     'Weiss Settings',
+//     'manage_options',
+//     'weisscp_admin_settings',
+//     'weisscp_admin_settings_page'
+//    );
+//  }
+
+
+ class weisscp_options_page {
+
+ 	function __construct() {
+ 		add_action( 'admin_menu', array( $this, 'weisscp_admin_settings' ) );
+ 	}
+
+ 	function weisscp_admin_settings() {
+ 		add_options_page(
+ 			'Weiss Settings Page',
+ 			'Weiss Settings',
+ 			'manage_options',
+ 			'weisscp_admin_settings',
+ 			array(
+ 				$this,
+ 				'weisscp_admin_settings_page'
+ 			)
+ 		);
+ 	}
+
+ 	function  weisscp_admin_settings_page() {
+ 		echo 'This is the page content';
+ 	}
  }
+
+ new weisscp_options_page;
