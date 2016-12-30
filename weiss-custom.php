f7cd24dc1cef786d19ffd31cbb96d53f5b09d05d
Li-Shann Tomchik <lili@roguedzines.com>
2016-12-30 11:05:57 -0500

    registration, install and setting

--- weiss-custom.php
+++ weiss-custom.php
@@ -11,6 +11,27 @@ Author: Li-Shann Tomchik
 Version: 1.0
 Author URI: http://weissinc.com
 */
-function hello(){
-  echo 'Hello World';
+
+/**
+* Prefix
+* weisscp_
+*/
+
+// check wordpress version before install
+register_activation_hook( __FILE__, 'weisscp_install' );
+
+function weisscp_install() {
+  if ( version_compare( get_bloginfo( 'version' ), '3.1', '<' ) ) {
+    deactivate_plugins( basename( __FILE__ ) ); // Deactivate our plugin
+  }
 }
+add_action( 'admin_menu', 'weisscp_admin_settings_page' );
+function weisscp_admin_settings_page() {
+  add_options_page(
+    'Weiss Settings',
+    'Weiss Settings',
+    'manage_options',
+    'weisscp_admin_settings',
+    'weisscp_admin_settings_page'
+   );
+ }
